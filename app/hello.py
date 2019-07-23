b86d19b41050c3537761b7beed45812c1dc6f31f
Niels Buekers <niels.buekers@gmail.com>
2019-07-23 10:48:21 +0200

    listen in PORT env

--- app/hello.py
+++ app/hello.py
@@ -47,7 +47,8 @@ def load():
 
 
 def main():
-    app.run(debug=False, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0",port=port)
 
 
 if __name__ == "__main__":
